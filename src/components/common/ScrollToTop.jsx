90362c17084f5cb3d8bc2e07cf72baef6ed6ecc9
poohghg <38716228+poohghg@users.noreply.github.com>
2021-09-03 14:45:37 +0900

    initial commit

--- src/components/common/ScrollToTop.jsx
+++ src/components/common/ScrollToTop.jsx
@@ -2,12 +2,12 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 const ScrollToTop = () => {
-    // 페이지 이동시 페이의 scrool을 0으로 이동
-    const pathName = useLocation().pathname;
-    useEffect(()=>{
-        window.scrollTo(0,0);
-    },[pathName]);
-    return null;
-}
+  // 페이지 이동시 페이지의 scrool을 0으로 이동
+  const pathName = useLocation().pathname;
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathName]);
+  return null;
+};
 
-export default ScrollToTop; 
+export default ScrollToTop;
