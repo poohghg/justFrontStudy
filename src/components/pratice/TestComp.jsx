35a0fe5eaa5f71a0004522e2930112f1f7dd9c9b
poohghg <38716228+poohghg@users.noreply.github.com>
2021-10-04 18:29:53 +0900

    commit

--- src/components/pratice/TestComp.jsx
+++ src/components/pratice/TestComp.jsx
@@ -1,11 +1,27 @@
 import React from "react";
-import {NM} from "../commonJS/common"
+import PropTypes from "prop-types";
+import { NM } from "../commonJS/common";
 
-const TestComp = React.memo(()=>{
-    return(
-        <div>
-            {NM}
-        </div>
-    )    
-})
+const PrTest = ({ name, children }) => {
+  // props children값을 통해 태그사이의 값을 표현 할 수 있다.
+  return (
+    <>
+      <div>{name}</div>
+      <div>{children}</div>
+    </>
+  );
+};
+
+const TestComp = React.memo(({ name }) => {
+  return <PrTest name={name}>3156</PrTest>;
+});
+
+TestComp.defaultProps = {
+  name: NM
+};
+// propTypes 를 통해 기본적인 타입을 지정해 줄 수 있다.
+TestComp.propTypes = {
+  name: PropTypes.string.isRequired
+};
+
 export default TestComp;
