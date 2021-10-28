7fbc1195ee5d297d9fe69134a2f621a7d4058ae7
poohghg <38716228+poohghg@users.noreply.github.com>
2021-10-28 10:58:58 +0900

    commit

--- src/components/talk/TalkBoard.jsx
+++ src/components/talk/TalkBoard.jsx
@@ -1,33 +1,13 @@
 import React, { useCallback, useEffect, useRef, useState } from "react";
 import styled, { css } from "styled-components";
-import { TalkTilte } from "./talkComp/TalkCommonComp";
-import TalkSwiper from "./common/TalkSwiper";
+import { TalkTilte, LongScrollComp } from "./talkComp/TalkCommonComp";
 
 const TalkBoardWrap = styled.div`
   width: 100%;
   background-color: #f0f0f0;
+  height: 100vh;
+  padding: 1rem;
 `;
-
-const SwiperWrap = styled.div`
-  position: relative;
-  width: 75%;
-  margin: 1rem auto;
-  height: auto;
-`;
-
-const swiperStyled = {
-  border: "1px solid #fff",
-  borderRadius: "16px",
-  height: "200px",
-  backgroundColor: "#fff"
-};
-
-const optionParams = {
-  spaceBetween: 30,
-  slidesPerView: 1.0
-  // resistance: false
-  // slidesOffsetAfter: 0
-};
 
 const data = [{ text: 1 }, { text: 2 }, { text: 3 }, { text: 4 }, { text: 5 }];
 
@@ -35,14 +15,7 @@ const TalkBoard = () => {
   return (
     <TalkBoardWrap>
       <div>맨위 공통 헤더</div>
-      <TalkTilte />
-      <SwiperWrap>
-        <TalkSwiper
-          datas={data}
-          style={swiperStyled}
-          optionParams={optionParams}
-        />
-      </SwiperWrap>
+      <LongScrollComp datas={data} />
     </TalkBoardWrap>
   );
 };
--- src/components/talk/talkComp/TalkCommonComp.jsx
+++ src/components/talk/talkComp/TalkCommonComp.jsx
@@ -12,15 +12,49 @@ const FlexBox = styled.div`
 `;
 
 const TalkTitleDiv = styled.div`
-  margin-top: 1.5vh;
+  margin: 2.5vh 0;
   font-size: 1rem;
   font-weight: bolder;
   display: flex;
   justify-content: space-between;
 `;
 
+const LongScrollDiv = styled.div`
+  display: flex;
+  flex-wrap: nowrap;
+  overflow-x: auto;
+  -ms-overflow-style: none;
+  ::-webkit-scrollbar {
+    display: none;
+    width: 0;
+  }
+`;
+
+const BestTalkDiv = styled(FlexBox)`
+  height: 20vh;
+  border: 1px solid #fff;
+  border-radius: 16px;
+  flex: 0 0 90%;
+  background-color: #fff;
+  margin-right: 3vw;
+`;
+
 styled.data``;
 
-export const TalkTilte = React.memo(() => {
-  return <TalkTitleDiv> 이번주 베스트 리뷰</TalkTitleDiv>;
+export const TalkTilte = React.memo(({ printStr }) => {
+  return <TalkTitleDiv> {printStr} </TalkTitleDiv>;
+});
+
+export const LongScrollComp = React.memo(({ datas }) => {
+  return (
+    <>
+      <TalkTilte printStr="이번주 베스트 리뷰" />
+      <LongScrollDiv>
+        {datas.length !== 0 &&
+          datas.map((data) => {
+            return <BestTalkDiv> {Object.values(data)[0]}</BestTalkDiv>;
+          })}
+      </LongScrollDiv>
+    </>
+  );
 });
